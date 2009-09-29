6c6e1d4fbf17b646dff7c559ebaaade161f28bfb
dsingh <dsingh@5673ff4f-8e2d-efd9-b3e6-cba0bb00debb>
2009-09-29 00:02:51 +0000

    AAMAS2010: Updated test01v3gm and test05v3gm results

--- doc/writeup/2010AAMAS/aamas10bdilearning/data/make_tikzdata.sh
+++ doc/writeup/2010AAMAS/aamas10bdilearning/data/make_tikzdata.sh
@@ -8,15 +8,15 @@
 SCRIPTDIR="$(cd "${0%/*}" 2>/dev/null; echo "$PWD")"
 PLOTSH=${SCRIPTDIR}/../../../../../src/distfiles/scripts/plot.sh
 
-##### Results 128 #####
-RESULTS110=${SCRIPTDIR}/../../../../discussion/dp1-new-worlds/results/blak128
-${PLOTSH} -d ${RESULTS110} -t test01v3gm -o ./test01v3gm -w 100 -z 250
-${PLOTSH} -d ${RESULTS110} -t test05v3gm -o ./test05v3gm -w 100 -z 250
+##### Results 130 #####
+RESULTS=${SCRIPTDIR}/../../../../discussion/dp1-new-worlds/results/blak130
+${PLOTSH} -d ${RESULTS} -t test01v3gm -o ./test01v3gm -w 100 -z 250
+${PLOTSH} -d ${RESULTS} -t test05v3gm -o ./test05v3gm -w 100 -z 250
 
 ##### Results 110 #####
-RESULTS110=${SCRIPTDIR}/../../../../discussion/dp1-new-worlds/results/blak110
-#${PLOTSH} -d ${RESULTS110} -t test01v10gm -o ./test01v10gm -w 100 -z 600
-#${PLOTSH} -d ${RESULTS110} -t test05v5gm -o ./test05v5gm -w 100 -z 400
-${PLOTSH} -d ${RESULTS110} -t testImpactvars -o ./testImpactvars -w 100 -z 300
-${PLOTSH} -d ${RESULTS110} -t testMultiSolutionsR -o ./testMultiSolutionsR -w 100 -z 1000
+RESULTS=${SCRIPTDIR}/../../../../discussion/dp1-new-worlds/results/blak110
+#${PLOTSH} -d ${RESULTS} -t test01v10gm -o ./test01v10gm -w 100 -z 600
+#${PLOTSH} -d ${RESULTS} -t test05v5gm -o ./test05v5gm -w 100 -z 400
+${PLOTSH} -d ${RESULTS} -t testImpactvars -o ./testImpactvars -w 100 -z 300
+${PLOTSH} -d ${RESULTS} -t testMultiSolutionsR -o ./testMultiSolutionsR -w 100 -z 1000
 #######################
