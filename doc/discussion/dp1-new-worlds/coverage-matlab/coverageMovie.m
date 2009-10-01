cc79bd2a5100e41c9f799c91976fa42ef91cb65c
dsingh <dsingh@5673ff4f-8e2d-efd9-b3e6-cba0bb00debb>
2009-10-01 03:26:33 +0000

    Coverage surface animated GIF

--- doc/discussion/dp1-new-worlds/coverage-matlab/coverageMovie.m
+++ doc/discussion/dp1-new-worlds/coverage-matlab/coverageMovie.m
@@ -1,24 +1,43 @@
 function coverageMovie ()
 
-fig=figure;
-set(fig,'Color', 'white');
+set(gcf,'Color', 'white');
 grid on;
-
-aviobj = avifile('coverage-surface.avi');
-aviobj.quality = 100;
-aviobj.fps = 15;
-
 [c,p] = meshgrid(0:.05:1,0:.05:1);
 z=0.5+(c.*(p-0.5));
-h = surf(c,p,z);
-
-for k=1:.5:25
-    hold on
-    axis([0 1 0 1 0 1])
-    view(45+k,37-k);
-    hold off
-    F = getframe(fig);
-    aviobj = addframe(aviobj,F);
+surf(c,p,z)
+view(20,12);
+pause(0.5);
+f = getframe(gcf);
+[im,map] = rgb2ind(f.cdata,256,'dither');
+ 
+i=1;
+r=30;
+x0=50;
+y0=12;
+for x=-r:2:r
+    s=solve('x^2 + y^2 = r^2',['r=' num2str(r)],['x=' num2str(x)]);
+    if (length(s.y) > 1)
+        y=max(double(s.y(2)),double(s.y(1)))/3;
+    else
+        y=double(s.y(1))/3;
+    end        
+    view(x0+x,y0+y);
+	f = getframe(gcf);
+    im(:,:,1,i) = rgb2ind(f.cdata,map,'dither');
+    i=i+1;
 end
-%close(fig)
-aviobj = close(aviobj);
+for x=r:-2:-r
+    s=solve('x^2 + y^2 = r^2',['r=' num2str(r)],['x=' num2str(x)]);
+    if (length(s.y) > 1)
+        y=min(double(s.y(2)),double(s.y(1)))/3;
+    else
+        y=double(s.y(1))/3;
+    end        
+    view(x0+x,y0+y);
+	f = getframe(gcf);
+    im(:,:,1,i) = rgb2ind(f.cdata,map,'dither');
+    i=i+1;
+end
+
+close(gcf);
+imwrite(im,map,'coverage-surface.gif','DelayTime',0,'LoopCount',inf)
