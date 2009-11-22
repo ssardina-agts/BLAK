fc13244ba020dc31b2cb221a139b2a74b20f9a0c
dsingh <dsingh@5673ff4f-8e2d-efd9-b3e6-cba0bb00debb>
2009-11-22 23:34:16 +0000

    hanoi: upport for complexity based confidence

--- domains/hanoi/src/PlanIdInfo.java
+++ domains/hanoi/src/PlanIdInfo.java
@@ -4,20 +4,14 @@ public class PlanIdInfo extends PlanInstanceInfo{
 
 	public int plan_id;
 	public double pSuccess;
-	public double coverage;
-	public double coverageWeight;
+	public double confidence;
 	public boolean isFailedThresholdHandler;
 
-	public PlanIdInfo(int id, double prob){
-		this(id, prob, 0.0, 1.0, false);
-	}
-
-	public PlanIdInfo(int id, double prob, double cov, double weight, boolean isFTH){
+	public PlanIdInfo(int id, double prob, double conf, boolean isFTH){
 		super(5/*default precedence*/);
 		plan_id = id;
 		pSuccess = prob;
-		coverage = cov;
-		coverageWeight = weight;
+		confidence = conf;
 		isFailedThresholdHandler = isFTH;
 	}
 }
