719373eb8102cde734e7cfb87fa99c111f6d55c8
dsingh <dsingh@5673ff4f-8e2d-efd9-b3e6-cba0bb00debb>
2009-11-29 03:30:46 +0000

    hycas: updated individual level results

--- doc/writeup/2010HYCAS/data/make_tikzdata.sh
+++ doc/writeup/2010HYCAS/data/make_tikzdata.sh
@@ -9,8 +9,8 @@ SCRIPTDIR="$(cd "${0%/*}" 2>/dev/null; echo "$PWD")"
 PLOTSH=${SCRIPTDIR}/../../../../src/distfiles/scripts/plot.sh
 
 
-##### Results 280 #####
-RESULTS=${SCRIPTDIR}/../../../results/blak280
+##### Results 291 #####
+RESULTS=${SCRIPTDIR}/../../../results/blak291
 ${PLOTSH} -d ${RESULTS} -t hanoid5s1r8 -o ./hanoid5s1r8 -w 100 -z 450
 ${PLOTSH} -d ${RESULTS} -t hanoid5s3r8 -o ./hanoid5s3r8 -w 100 -z 450
 ${PLOTSH} -d ${RESULTS} -t hanoid5s5r8 -o ./hanoid5s5r8 -w 100 -z 450
