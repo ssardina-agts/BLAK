eb0b606f766573e693b8e73144db705434d4eaea
dsingh <dsingh@5673ff4f-8e2d-efd9-b3e6-cba0bb00debb>
2010-01-28 05:25:22 +0000

    icml: updated basic results

--- doc/writeup/2010ICML/Data/make_tikzdata.sh
+++ doc/writeup/2010ICML/Data/make_tikzdata.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
-#####
+###
 # Script to generate TikZ data files for the results specified
 # Author: dsingh
-#####
+#
 
 SCRIPTDIR="$(cd "${0%/*}" 2>/dev/null; echo "$PWD")"
 PLOTSH=${SCRIPTDIR}/../../../../src/distfiles/scripts/plot.sh
 
 
-##### Add line numbers (tcsh format) 
-#foreach file ( `find . -name "*csv" -print` )
-#awk '{print FNR, $0}' $file > $file.tmp
-#mv $file.tmp $file
-#end
+### Make results #####
+RESULTS=${SCRIPTDIR}/../../../results/blak343/blak/results/all
 
+### Add line numbers to the raw data files:
+for file in `find ${RESULTS} -name "testfr*topgoal.csv" -print`
+do
+file1=$(basename $file)
+awk '{print FNR, $0}' $file > ${SCRIPTDIR}/$file1
+done
 
-##### Make results #####
-RESULTS=${SCRIPTDIR}/.
-${PLOTSH} -d ${RESULTS} -t testfr01a -o ./testfr01a -w 5 -z 100
-${PLOTSH} -d ${RESULTS} -t testfr01b -o ./testfr01b -w 5 -z 100
-${PLOTSH} -d ${RESULTS} -t testfr01na -o ./testfr01na -w 5 -z 100
-${PLOTSH} -d ${RESULTS} -t testfr01nb -o ./testfr01nb -w 5 -z 100
+### Generate the data files for tikZ
+${PLOTSH} -d ${SCRIPTDIR} -t testfr01a -o ./testfr01a -w 5 -z 50
+${PLOTSH} -d ${SCRIPTDIR} -t testfr01b -o ./testfr01b -w 5 -z 50
+${PLOTSH} -d ${SCRIPTDIR} -t testfr01na -o ./testfr01na -w 5 -z 50
+${PLOTSH} -d ${SCRIPTDIR} -t testfr01nb -o ./testfr01nb -w 5 -z 50
 
-
-### Take the first N samples ####
+### Take first N samples for plotting since size can vary
 HEAD=10
 for file in `find . -name "*tikzdata" -print`
 do
-head -${HEAD} $file > $file.tmp
+head -${HEAD} $file > ${SCRIPTDIR}/$file.tmp
 mv $file.tmp $file
 done
+
+### Remove the temp files
+for file in `find ${RESULTS} -name "testfr*topgoal.csv" -print`
+do
+file1=$(basename $file)
+/bin/rm ${SCRIPTDIR}/$file1
+done
+
+###
