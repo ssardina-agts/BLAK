1f601784ca2453506412e7e8351d87bbac8aee0d
dsingh <dsingh@5673ff4f-8e2d-efd9-b3e6-cba0bb00debb>
2010-04-06 02:36:28 +0000

    hycas2010: updated results

--- doc/writeup/2010HYCAS/data/make_tikzdata.sh
+++ doc/writeup/2010HYCAS/data/make_tikzdata.sh
@@ -6,21 +6,29 @@
 #####
 
 SCRIPTDIR="$(cd "${0%/*}" 2>/dev/null; echo "$PWD")"
-PLOTSH=${SCRIPTDIR}/../../../../src/distfiles/scripts/plot.sh
+#PLOTSH=${SCRIPTDIR}/../../../../src/distfiles/scripts/plot.sh
+PLOTSH=./plot.sh
 
-function scaleResults () { mv $1 .tmpfile && awk '{ print $1/2 " " $2}'  .tmpfile > $1 && rm .tmpfile ; }
+function scaleXResults () { mv $1 .tmpfile && awk '{ print $1/2 " " $2}'  .tmpfile > $1 && rm .tmpfile ; }
+function scaleYResults () { mv $1 .tmpfile && awk '{ print $1 " " $2/2}'  .tmpfile > $1 && rm .tmpfile ; }
 
 
 ##### Results 295 #####
-RESULTS=${SCRIPTDIR}/../../../results/blak295
+#RESULTS=${SCRIPTDIR}/../../../results/blak295
+RESULTS=${SCRIPTDIR}/../../../results/blak295+
+
 ${PLOTSH} -d ${RESULTS} -t hanoid5s1r8 -o ./hanoid5s1r8 -w 100 -z 485
 ${PLOTSH} -d ${RESULTS} -t hanoid5s3r8 -o ./hanoid5s3r8 -w 100 -z 485
 ${PLOTSH} -d ${RESULTS} -t hanoid5s5r8 -o ./hanoid5s5r8 -w 100 -z 485
 ${PLOTSH} -d ${RESULTS} -t hanoi5 -o ./hanoi5 -w 200 -z 1975
 ${PLOTSH} -d ${RESULTS} -t hanoi5t -o ./hanoi5t -w 200 -z 1975
 
+${PLOTSH} -d ${RESULTS} -t hanoi5s -o ./hanoi5s -w 1 -z 5
+
 # Tex cannot handle >16383 numbers so scale results here
-scaleResults hanoi5.CF.tikzdata
-scaleResults hanoi5.CP.tikzdata
-scaleResults hanoi5t.CF.tikzdata
-scaleResults hanoi5t.CP.tikzdata
+scaleXResults hanoi5.CF.tikzdata
+scaleXResults hanoi5.CP.tikzdata
+scaleXResults hanoi5t.CF.tikzdata
+scaleXResults hanoi5t.CP.tikzdata
+scaleYResults hanoi5s.CF.tikzdata
+scaleYResults hanoi5s.CP.tikzdata
