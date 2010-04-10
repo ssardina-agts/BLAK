d02d3fd245b005fc53828176e3a9facac252c65e
dsingh <dsingh@5673ff4f-8e2d-efd9-b3e6-cba0bb00debb>
2010-04-10 02:30:38 +0000

    hycas2010: final set of rebuttal changes

--- doc/writeup/2010HYCAS/data/make_tikzdata.sh
+++ doc/writeup/2010HYCAS/data/make_tikzdata.sh
@@ -20,7 +20,7 @@ RESULTS=${SCRIPTDIR}/../../../results/blak295+
 ${PLOTSH} -d ${RESULTS} -t hanoid5s1r8 -o ./hanoid5s1r8 -w 100 -z 485
 ${PLOTSH} -d ${RESULTS} -t hanoid5s3r8 -o ./hanoid5s3r8 -w 100 -z 485
 ${PLOTSH} -d ${RESULTS} -t hanoid5s5r8 -o ./hanoid5s5r8 -w 100 -z 485
-${PLOTSH} -d ${RESULTS} -t hanoi5 -o ./hanoi5 -w 200 -z 1975
+${PLOTSH} -d ${RESULTS} -t hanoi5 -o ./hanoi5 -w 200 -z 1900
 ${PLOTSH} -d ${RESULTS} -t hanoi5t -o ./hanoi5t -w 200 -z 1975
 
 ${PLOTSH} -d ${RESULTS} -t hanoi5s -o ./hanoi5s -w 1 -z 5
