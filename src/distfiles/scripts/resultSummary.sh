66031887c14f702518852cdd0acb9a2bfc365cf5
dsingh <dsingh@5673ff4f-8e2d-efd9-b3e6-cba0bb00debb>
2010-08-19 06:25:10 +0000

    Added resultSummary.sh

---
+++ src/distfiles/scripts/resultSummary.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#--- Parse arguments
+if [ "$2" == "" ]; then 
+echo "usage: $0 <path-to-results-root-dir> <output-dir>"
+exit
+fi
+path=$1
+outpath=$2
+
+#--- Helper function
+getTreeInfo()
+{
+infile=$1
+tail -10000 $infile | egrep "number of instances|Number of Leaves|Size of the tree|Outcomes for"
+}
+
+getTries()
+{
+infile=$1
+tmpfile=tmpfile$RANDOM.txt
+grep "Recorded success for goal" $1 | cut -f1 -d'.' | sort | uniq  > $tmpfile
+cat $tmpfile | while read line; do 
+line2=`echo $line | sed -e 's/\[/\\\[/g' | sed -e 's/\]/\\\]/g'`
+sed -n "/$line2/{p;q;}" $infile
+done
+rm $tmpfile
+}
+
+#--- Main 
+for file in `find $1 -name "player.out" -print | grep concurrent`
+do 
+file2=$outpath/`echo $file | sed -e 's/.*results\///' | sed -e 's/\//-/g'`.txt
+echo $file
+getTreeInfo $file > $file2
+getTries $file >> $file2
+done
+exit
+
+
