d91db8182411cebd7365a5079a488ae508a5fc7c
dsingh <dsingh@5673ff4f-8e2d-efd9-b3e6-cba0bb00debb>
2010-09-16 03:45:51 +0000

    aamas2011: updated application section. added results

--- doc/writeup/2011AAMAS/paper/data/make_tikzdata.sh
+++ doc/writeup/2011AAMAS/paper/data/make_tikzdata.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
-#####
 # Script to generate TikZ data files for the results specified
-# Author: dsingh
-#####
+# Author: Dhirendra Singh
 
 SCRIPTDIR="$(cd "${0%/*}" 2>/dev/null; echo "$PWD")"
 PLOTSH=${SCRIPTDIR}/../../../../../src/distfiles/scripts/plot.sh
 
 
-##### Results 181 #####
-#RESULTS=${SCRIPTDIR}/../../../../results/blak181
-#${PLOTSH} -d ${RESULTS} -t test01v3gm -o ./test01v3gm -w 100 -z 150
+##### Results 468 #####
+RESULTS=${SCRIPTDIR}/../../../../results/blak468
+${PLOTSH} -d ${RESULTS} -t storage1 -o ./storage1b -w 500 -z 200
+${PLOTSH} -d ${RESULTS} -t storage1m -o ./storage1mb -w 500 -z 200
+${PLOTSH} -d ${RESULTS} -t storage2b -o ./storage2b -w 500 -z 200
+${PLOTSH} -d ${RESULTS} -t storage2mb -o ./storage2mb -w 500 -z 200
