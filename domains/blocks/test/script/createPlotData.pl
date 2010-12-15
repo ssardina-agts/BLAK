8b9b084f343d61c40161249032d032783f83574a
s9609787 <s9609787@5673ff4f-8e2d-efd9-b3e6-cba0bb00debb>
2010-12-15 05:11:48 +0000

    blocks: fixed plotting

--- domains/blocks/test/script/createPlotData.pl
+++ domains/blocks/test/script/createPlotData.pl
@@ -5,11 +5,11 @@ $experimentTimes = $ARGV[1];
 $content ="";
 $content .= "set terminal postscript landscape color rounded\n";
 $content .= "set grid\n";
-$content .= "set xrange [0:3]\n";
-$content .= 'set xtics ("TrainStepsAverage" 1, "LearnStepsAverage" 2)'."\n";
-$content .= 'set xlabel "Run times"'."\n";
-$content .= 'set ylabel "Steps used"'."\n";
-$content .= 'set output "'.$filename.'.pdf"'."\n" ;	
+#$content .= "set xrange [0:3]\n";
+$content .= 'set xtics ("Training (Random Selection)" 1, "Testing (Learned Selection)" 2)'."\n";
+#$content .= 'set xlabel "Run times"'."\n";
+$content .= 'set ylabel "Moves (Average)"'."\n";
+$content .= 'set output "'.$filename.'.eps"'."\n" ;	
 
 $trainSum = 0;
 $learnSum = 0;
@@ -28,14 +28,15 @@ $learnAverage = $learnSum/$experimentTimes; # change
 $min = int($learnAverage) - 1;
 $max = int($trainAverage) + 2;
 
-$content .="set yrange [$min:$max]\n";
-$content .= 'plot [0:3] "'.$filename.'" with boxes'."\n";
+$content .="set yrange [0:$max]\n";
+$content .= 'plot [0:3] "'.$filename.'" with boxes fs solid lc rgb "blue"'."\n";
 $content .= "\n";
 
 $width = 0.4;
 
 open(FH,">",$filename)|| die "Can not that file";
-print FH "0\t$min\t$width\n1\t$trainAverage\t$width\n2\t$learnAverage\t$width\n3\t$min\t$width\n";
+#print FH "0\t$min\t$width\n1\t$trainAverage\t$width\n2\t$learnAverage\t$width\n3\t$min\t$width\n";
+print FH "1\t$trainAverage\t$width\n2\t$learnAverage\t$width\n";
 close FH;
 
 #create plot script
